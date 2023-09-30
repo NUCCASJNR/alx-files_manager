59b4ef169be076e7c7e1ddd77865a743ad864b03
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-30 16:39:01 +0100

    status code

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -12,13 +12,13 @@ class AppController {
   static getStatus (req, res) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
-    res.json({ redis: redisStatus, db: dbStatus });
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
   static async getStats (req, res) {
     const nbUsers = await dbClient.nbUsers();
     const nbFiles = await dbClient.nbFiles();
-    res.json({ users: nbUsers, files: nbFiles });
+    res.status(200).json({ users: nbUsers, files: nbFiles });
   }
 }
 
