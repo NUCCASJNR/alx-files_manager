f038a04b40f5832f630d3e6199516bdf41d38434
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-10-01 09:48:07 +0100

    running not listening

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 MapRoutes(app);
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
